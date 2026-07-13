a3bef96338bfb5317cc0bd9095f6c41588206385
nhattran998 <nhattq.coding@gmail.com>
2026-07-13 21:09:19 +0700

    feat(usage): add getMemberStats repo fn with TPS stats

--- src/lib/db/index.js
+++ src/lib/db/index.js
@@ -59,7 +59,7 @@ export {
 export {
   statsEmitter, trackPendingRequest, getActiveRequests,
   saveRequestUsage, getUsageHistory, getUsageStats, getChartData,
-  appendRequestLog, getRecentLogs,
+  appendRequestLog, getRecentLogs, getMemberStats,
 } from "./repos/usageRepo.js";
 
 // Request details
